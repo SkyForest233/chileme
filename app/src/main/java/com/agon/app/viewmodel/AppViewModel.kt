aed8d02550867f27feb070f9900d80dc582f9c94
SkyForest233 <50165738+SkyForest233@users.noreply.github.com>
2026-09-15 21:05:31 +0800

    Update AppViewModel.kt

--- app/src/main/java/com/agon/app/viewmodel/AppViewModel.kt
+++ app/src/main/java/com/agon/app/viewmodel/AppViewModel.kt
@@ -299,7 +299,6 @@ class AppViewModel(application: Application) : AndroidViewModel(application) {
 
     fun restoreArchived(id: String) = viewModelScope.launch { repo.restoreArchived(id) }
 
-
     fun deleteArchived(id: String) = viewModelScope.launch { repo.deleteArchived(id) }
 
     fun clearArchive() = viewModelScope.launch { repo.clearArchive() }
